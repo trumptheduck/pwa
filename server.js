0be9e9691d1799e991b037d0934f50c737e97af1
unknown <nhatyt123@gmail.com>
2021-01-13 05:05:28 +0700

    Well basically open beta

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
-const express = require('express')
-const path = require('path')
-
+const express = require('express');
+const app = express()
+const http = require('http').Server(app);
+const path = require("path")
+const io = require('socket.io')(http);
 const httpPort = 80
 
-const app = express()
-
+io.on('connection', (socket)=> {
+  console.log("An user connected!")
+  socket.emit('status', "You are connected")
+  socket.on('request', (data) => {
+    console.log("An user requested a package!")
+    socket.emit('response', "Well fuck you!")
+  })
+})
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
-app.listen(httpPort, function () {
+http.listen(httpPort, function () {
   console.log(`Listening on port ${httpPort}!`)
 })
